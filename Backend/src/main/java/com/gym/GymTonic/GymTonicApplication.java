b749ae5a5b0b3d1211d117e4332af4b9c331cdf8
Tiberiuss <32412228+Tiberiuss@users.noreply.github.com>
2023-06-21 12:45:26 +0200

    H2 Server and CSV parsing fixes

--- Backend/src/main/java/com/gym/GymTonic/GymTonicApplication.java
+++ Backend/src/main/java/com/gym/GymTonic/GymTonicApplication.java
@@ -7,14 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GymTonicApplication implements ApplicationRunner {
+public class GymTonicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymTonicApplication.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		CSVHelper.initDB();
 	}
 }
--- Backend/src/main/java/com/gym/GymTonic/helper/CSVHelper.java
+++ Backend/src/main/java/com/gym/GymTonic/helper/CSVHelper.java
@@ -1,22 +1,34 @@
 package com.gym.GymTonic.helper;
 
+import com.gym.GymTonic.model.Exercise;
+import com.gym.GymTonic.model.Material;
+import com.gym.GymTonic.model.Muscle;
+import com.gym.GymTonic.repository.ExerciseRepository;
+import com.gym.GymTonic.service.ExerciseService;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
+import javax.annotation.PostConstruct;
 import java.io.FileReader;
 import java.io.IOException;
 
+@Component
 public class CSVHelper {
-    public static void initDB(){
+
+    private ExerciseService exerciseService;
+
+    @PostConstruct
+    private void initDB(){
         String filename = "classpath:workout-data.csv";
 
         try (CSVParser csvParser = new CSVParser(new FileReader(ResourceUtils.getFile(filename)), CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
             Iterable<CSVRecord> csvRecords = csvParser.getRecords();
-
             for (CSVRecord csvRecord : csvRecords) {
-                System.out.println(csvRecord.get("exercise_name"));
+               // System.out.println(new Exercise(csvRecord.get("exercise_name"), Material.valueOf(csvRecord.get("Category"))));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
