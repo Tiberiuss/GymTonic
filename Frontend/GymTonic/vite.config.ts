50cb02d9f18e7e74d0cd686730d03cde75633721
Tiberiuss <32412228+Tiberiuss@users.noreply.github.com>
2023-06-23 13:11:41 +0200

    Dockerize app

--- Frontend/GymTonic/vite.config.ts
+++ Frontend/GymTonic/vite.config.ts
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    preview: {
+      host: env.HOST,
+      port: Number(env.PORT) || 3000,
+    },
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
