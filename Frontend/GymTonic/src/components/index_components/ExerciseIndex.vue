63fc4545ad02f78b7956c10f75fde7a179e68685
loreenzo02 <ocaloalex@gmail.com>
2023-06-28 13:08:59 +0200

    Index completed

--- Frontend/GymTonic/src/components/index_components/ExerciseIndex.vue
+++ Frontend/GymTonic/src/components/index_components/ExerciseIndex.vue
@@ -13,113 +13,120 @@
         <div class="img">
             <img />
         </div>
-        <div class="exercise-extra">
-            <p class="exercise-title">
-                <slot name="exercise-title"></slot>
-            </p>
-                <a class="description-icon">
-                    <IconExerciseDescription></IconExerciseDescription>
-                </a>
-            <p></p>
-            <a class="progress-icon">
-                <IconExerciseProgress></IconExerciseProgress>
-            </a>
-        </div>
-        <div class="exercise-muscles" v-for="muscle in muscles">
-            <p>
-                {{ muscle['muscle_name'] }}
-            </p>
+        <div class="exercise-desc">
+            <div class="exercise-extra">
+                <p class="exercise-title">
+                    <slot name="exercise-title"></slot>
+                </p>
+                <div class="exercise-icons">
+                    <a class="description-icon">
+                        <IconExerciseDescription></IconExerciseDescription>
+                    </a>
+                    <a class="progress-icon">
+                        <IconExerciseProgress></IconExerciseProgress>
+                    </a>
+                </div>
+            </div>
+            <div class="exercise-muscles">
+                <p class="muscle-text" v-for="muscle in muscles">
+                    {{ muscle['muscle_name'] }}
+                </p>
+            </div>
         </div>
     </section>
 </template>
 <style>
     .exercise {
-        background-color: #CC6F1A;
-        border-top-left-radius: 20px;
-        border-bottom-right-radius: 20px;
-        margin-top: 10%;
-        height: 150px;
-        width: 40vw;
-        display: flex;
+        margin-top: 1%;
+        min-height: 200px;
+        display: grid;
+        grid-template-columns: 50% 50%;
     }
 
     .img {
-        width: 50%;
         background-color: rgb(76, 72, 72);
         border-top-left-radius: 20px;
-        height: 100%;
+    }
+
+    .exercise-desc {
+        background-color: #CC6F1A;
+        border-bottom-right-radius: 20px;
+        display: grid;
+        grid-template-rows: 60% 40%;
     }
 
     .exercise-extra {
-        position: absolute;
         display: grid;
-        grid-template-columns: 80% 20%;
-        margin-left: 22vw;
-        height: 60px;
-        width: 18vw;
-        margin-top: 5px;
+        grid-template-columns: 85% 15%;
+        grid-gap: 5px;
+        padding-top: 5px;
+        padding-left: 5px;
+        padding-right: 5px;
     }
 
     .exercise-muscles {
-        display: flex;
-        height: 60px;
-        margin-right: 4vh;
-        margin-left: 4vh;
-        margin-top: 80px;
+        display: grid;
+        grid-template-columns: 50% 50%;
+        column-gap: 1vw;
+        margin-right: 3vw;
+        margin-left: 1vw;
     }
 
     .exercise-title {
         display: inline;
-        font-size: 2vw;
+        font-size: 20px;
     }
 
+    .muscle-text {
+        background-color: black;
+        color: white;
+        border-radius: 20px;
+        text-align: center;
+        height: 20px;
+        font-size: 12px;
+    }
 
     @media screen and (max-width: 1000px) {
         .exercise {
-            width: 90vw;
-            height: 150px;
-        }
-
-        .exercise-extra {
-            margin-left: 47vw;
+            min-height: 200px;
+            display: grid;
+            grid-template-columns: 50% 50%;
         }
 
         .exercise-title {
             display: inline;
-            font-size: 30px;
+            font-size: 17px;
         }
 
         .exercise-extra {
-            position: absolute;
             display: grid;
             grid-template-columns: 80% 20%;
-            margin-left: 47vw;
-            height: 60px;
-            width: 43vw;
-            margin-top: 5px;
+            grid-gap: 10px;
+            padding-top: 5px;
+            padding-left: 5px;
         }
-    }
-
-    @media screen and (max-width: 550px) {
-
-    }
-
-    @media screen and (max-width: 450px) {
     }
 
     @media screen and (max-width: 300px) {
         .exercise-extra {
-            position: absolute;
             display: grid;
             grid-template-columns: 70% 30%;
-            margin-left: 47vw;
-            height: 60px;
-            width: 43vw;
-            margin-top: 5px;
+            grid-gap: 10px;
+            padding-top: 5px;
+            padding-left: 5px;
         }
 
         .exercise-title {
-            font-size: 20px;
+            font-size: 15px;
+        }
+
+        .muscle-text {
+            background-color: black;
+            color: white;
+            border-radius: 20px;
+            text-align: center;
+            height: 20px;
+            font-size: 9px;
         }
     }
 </style>
--- Frontend/GymTonic/src/components/index_components/GridExercisesComponent.vue
+++ Frontend/GymTonic/src/components/index_components/GridExercisesComponent.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
     import ExerciseIndex from './ExerciseIndex.vue';
+    import axios from 'axios';
 </script>
 <template>
-<div class="section-grid">
+<div v-if="!status" class="section-grid">
     <ExerciseIndex :muscles="element['muscle']" v-for="element in exercises">
         <template #exercise-title>{{ element['name'] }}</template>
     </ExerciseIndex>
+</div>
+<div v-else>
+    An error ocurred.
 </div>
 </template>
 
@@ -18,20 +22,12 @@ export default {
         }
     },
     async mounted() {
-        const response = await fetch(
-            'http://localhost:8080/api/v1/exercise',
-            {
-                method: 'GET',
-                headers: { "Content-type": "application/json; charset=UTF-8" },
-            }
-        );
-
-        if (response["status"] != 200 && response["status"] != 201) {
-          this.status = true;
-          return;
-        }
-
-        this.exercises = await response.json()
+        axios.get('http://localhost:8080/api/v1/exercise')
+        .then(response => (this.exercises = response.data))
+        .catch(error => {
+            console.log(error)
+            this.status = true
+        })
     }
 }
 </script>
@@ -40,11 +36,12 @@ export default {
 .section-grid {
     display: grid;
     grid-template-columns: 50% 50%;
+    grid-gap: 2vw;
     width: 90vw;
     height: 80vh;
 }
 
-@media screen and (max-width: 800px) {
+@media screen and (max-width: 700px) {
     .section-grid {
         display: grid;
         grid-template-columns: 100%;
